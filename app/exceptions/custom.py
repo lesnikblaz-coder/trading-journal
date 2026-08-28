b8f5b0c33f0fdc9e90badc1406a4ad83c4c81e1e
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-08-28 14:22:04 +0200

    add more custom errors for error handling

--- app/exceptions/custom.py
+++ app/exceptions/custom.py
@@ -1,6 +1,23 @@
 from app.exceptions.base import AppException
 
 
+
 class InvalidTokenError(AppException):
     status_code = 401
     detail = "Invalid token."
+
+class InvalidCredentialsError(AppException):
+    status_code = 401
+    detail = "Invalid authentication credentials."
+
+class UserNotFoundError(AppException):
+    status_code = 404
+    detail = "User not found."
+
+class DuplicateEmailError(AppException):
+    status_code = 409
+    detail = "Email already in use."
+
+class InsufficientPermissions(AppException):
+    status_code = 403
+    detail = "Insufficient permissions."
