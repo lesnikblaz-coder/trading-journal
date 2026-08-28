2e1a2e00520a835a9b95d7ef2d14968637445cc8
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-08-28 15:09:28 +0200

    create user repository, add get_by_email method and query

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -1,8 +1,16 @@
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from app.repositories.base import BaseRepo
+from app.models.user import User
 
 
-class UserRepo:
-    def __init__(self, session: AsyncSession):
-        self.session = session
+class UserRepo(BaseRepo[User]):
+    model = User
 
-    async def create(self):
+    async def get_by_email(self, email: str) -> User | None:
+        result = await self.session.execute(
+            select(User)
+            .where(User.email == email)
+        )
+
+        return result.scalar_one_or_none()
