d5693e98883b69e1cd84a0ffe9f1dd1ae1ff955d
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-08-30 20:26:30 +0200

    add shared get_by_id, update and delete queries in BaseRepo class

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -1,4 +1,5 @@
-from typing import Generic, TypeVar
+from typing import Generic, TypeVar, Sequence, Any
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -15,4 +16,40 @@ class BaseRepo(Generic[ModelT]):
     async def create(self, entity: ModelT) -> ModelT:
         self.session.add(entity)
         await self.session.flush()
+        return entity
+
+    async def get_all(self) -> Sequence[ModelT]:
+        result = await self.session.execute(select(self.model))
+
+        return result.scalars().all()
+
+    async def get_by_id(self, entity_id: int) -> ModelT | None:
+        result = await self.session.execute(
+            select(self.model)
+            .where(self.model.id == entity_id)
+        )
+
+        return result.scalar_one_or_none()
+
+    async def update(self, entity_id: int, **fields: Any) -> ModelT | None:
+        entity = await self.get_by_id(entity_id)
+
+        if entity is None:
+            return None
+
+        for field, value in fields.items():
+            setattr(entity, field, value)
+
+        await self.session.flush()
+
+        return entity
+
+    async def delete(self, entity_id: int) -> ModelT | None:
+        entity = await self.get_by_id(entity_id)
+
+        if entity is None:
+            return None
+
+        entity = await self.session.delete(entity)
+
         return entity
