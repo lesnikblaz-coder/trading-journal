401e3b85b83557caa72a69b5b3b862a02cbb7696
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-08-30 19:10:26 +0200

    remove auth and put it in a subfolder routers, include/register the router

--- app/main.py
+++ app/main.py
@@ -1,30 +1,19 @@
-from fastapi import FastAPI, Depends
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import FastAPI
 
 from app.exceptions.handler import register_exception_handlers
-from app.schemas import auth as auth_sc
-from app import dependencies as dep
+
+# routers
+from app.routers.auth import router as auth_router
 
 
 app = FastAPI()
 
 register_exception_handlers(app)
 
+app.include_router(auth_router)
+
+
 # ---------- ROOT ----------
 @app.get("/", include_in_schema=False)
 async def root() -> dict[str, str]:
     return {"status": "TRUE"}
-
-
-# ---------- AUTH ----------
-@app.post("/auth/register", response_model=auth_sc.TokenResponse)
-async def register(service: dep.AuthServiceDep, request: auth_sc.Register) -> auth_sc.TokenResponse:
-    return await service.register(request.email, request.password)
-
-@app.post("/auth/login", response_model=auth_sc.TokenResponse)
-async def login(service: dep.AuthServiceDep, request: auth_sc.Login) -> auth_sc.TokenResponse:
-    return await service.login(request.email, request.password)
-
-@app.post("/auth/token", response_model=auth_sc.TokenResponse)
-async def token(service: dep.AuthServiceDep, request: OAuth2PasswordRequestForm = Depends()):
-    return await service.login(request.username, request.password)
