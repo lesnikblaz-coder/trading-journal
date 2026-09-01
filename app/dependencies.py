629140d0353f75bd368ab0fd56a404325628f337
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-09-01 11:05:22 +0200

    add a dependency to get the current user auth

--- app/dependencies.py
+++ app/dependencies.py
@@ -6,6 +6,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.database.base import AsyncSessionLocal
 from app.repositories.user import UserRepo
 from app.services.auth import AuthService
+from app.database.models.user import User
+from app.core.security import oauth2_scheme
 
 
 # ---------- SESSION ----------
@@ -25,9 +27,17 @@ async def _get_user_repo(session: SessionDep) -> UserRepo:
 UserRepoDep = Annotated[UserRepo, Depends(_get_user_repo)]
 
 
-
 # ---------- AUTH ----------
+TokenDep = Annotated[str, Depends(oauth2_scheme)]
+
 async def _get_auth_service(repo: UserRepoDep) -> AuthService:
     return AuthService(repo)
 
 AuthServiceDep = Annotated[AuthService, Depends(_get_auth_service)]
+
+
+async def _get_current_user(token: TokenDep, auth_service: AuthServiceDep) -> User | None:
+    return await auth_service.decode_user(token)
+
+
+CurrentUserDep = Annotated[User, Depends(_get_current_user)]
