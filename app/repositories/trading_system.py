a752a7712c04a70822c062fd42e3ca27f182db44
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-09-01 21:50:30 +0200

    add a query to fetch data's trading systems

--- app/repositories/trading_system.py
+++ app/repositories/trading_system.py
@@ -1,5 +1,16 @@
+from uuid import UUID
+from typing import Sequence
+from sqlalchemy import select
+
 from app.repositories.base import BaseRepo
+from app.database.models.trading_system import TradingSystem
 
 
 class TradingSystemRepo(BaseRepo):
-    pass
+    async def get_by_user(self, user_id: UUID) -> Sequence[TradingSystem]:
+        result = await self.session.execute(
+            select(TradingSystem)
+            .where(TradingSystem.user_id == user_id)
+        )
+
+        return result.scalars().all()
