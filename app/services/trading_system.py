aadf8190ec0fbf9548be47cb2386c4bbb83944c3
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-09-01 21:51:23 +0200

    add create, get_by_user and get_by_id methods with model validation for responses etc.

--- app/services/trading_system.py
+++ app/services/trading_system.py
@@ -2,19 +2,29 @@ from uuid import UUID
 
 from app.repositories.trading_system import TradingSystemRepo
 from app.database.models.trading_system import TradingSystem
-from app.schemas.trading_system import TradingSystemRequest
+from app.schemas.trading_system import TradingSystemRequest, TradingSystemResponse
 
 
 class TradingSystemService:
     def __init__(self, trading_system_repo: TradingSystemRepo):
         self.repo = trading_system_repo
 
-    async def create(self, request: TradingSystemRequest, user_id: UUID):
+    async def create(self, request: TradingSystemRequest, user_id: UUID) -> TradingSystemResponse:
 
-        request_dict = request.model_dump(exclude_none=True)
-
-        request_dict["user_id"] = user_id
-
-        trading_system = TradingSystem(**request_dict)
+        trading_system = TradingSystem(
+            user_id=user_id,
+            **request.model_dump(exclude_none=True)
+        )
 
         return await self.repo.create(trading_system)
+
+    async def get_by_user(self, user_id: UUID) -> list[TradingSystemResponse]:
+        result = await self.repo.get_by_user(user_id)
+
+        return [TradingSystemResponse.model_validate(r) for r in result]
+
+    async def get_by_id(self, trading_system_id: UUID, user_id: UUID) -> TradingSystemResponse | None:
+        return await self.repo.get_by_id(
+            entity_id=trading_system_id,
+            user_id=user_id
+        )
