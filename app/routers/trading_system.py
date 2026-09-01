bf2bf5473945e4b08e1dd370ef819cab1d875b1e
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-09-01 21:52:32 +0200

    add get users all and get by id endpoints

--- app/routers/trading_system.py
+++ app/routers/trading_system.py
@@ -1,3 +1,4 @@
+from uuid import UUID
 from fastapi import APIRouter
 
 from app.schemas import trading_system as sc
@@ -8,5 +9,13 @@ router = APIRouter()
 
 
 @router.post("/trading-systems", response_model=sc.TradingSystemResponse)
-async def create_trading_system(service: dep.TradingSystemServiceDep, request: sc.TradingSystemRequest, user: dep.CurrentUserDep):
+async def create_trading_system(service: dep.TradingSystemServiceDep, request: sc.TradingSystemRequest, user: dep.CurrentUserDep) -> sc.TradingSystemResponse:
     return await service.create(request=request, user_id=user.id)
+
+@router.get("/trading-systems", response_model=list[sc.TradingSystemResponse])
+async def get_trading_systems(service: dep.TradingSystemServiceDep, user: dep.CurrentUserDep) -> list[sc.TradingSystemResponse]:
+    return await service.get_by_user(user_id=user.id)
+
+@router.get("/trading-systems/{trading_system_id}", response_model=sc.TradingSystemResponse)
+async def get_trading_system_by_id(trading_system_id: UUID, service: dep.TradingSystemServiceDep, user: dep.CurrentUserDep) -> sc.TradingSystemResponse | None:
+    return await service.get_by_id(trading_system_id=trading_system_id, user_id=user.id)
