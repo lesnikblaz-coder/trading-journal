5436db2bd0a9b84b1c21e9e74b6f6dbe52ba56e7
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-09-03 14:21:08 +0200

    finish add_trade endpoint, so a user can input it's own trades for selected trading system

--- app/routers/trade.py
+++ app/routers/trade.py
@@ -1,13 +1,19 @@
+from uuid import UUID
 from fastapi import APIRouter
 
 from app import dependencies as dep
+from app.schemas import trade as sc
 
 
 router = APIRouter()
 
 @router.post("/trading-systems/{system_id}/trades")
-async def add_trade():
-    pass
+async def add_trade(system_id: UUID, request: sc.TradeCreateRequest, service: dep.TradeServiceDep, user: dep.CurrentUserDep):
+    return await service.create(
+        system_id,
+        request,
+        user.id
+    )
 
 @router.get("/trading-systems/{system_id}/trades")
 async def get_user_trades():
