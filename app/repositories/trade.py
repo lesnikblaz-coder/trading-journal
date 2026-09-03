782d989811b25bc80d46492781fec2b699930128
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-09-03 14:43:46 +0200

    create get_all_for_system method that queries all trades for user for given trading system

--- app/repositories/trade.py
+++ app/repositories/trade.py
@@ -1,3 +1,7 @@
+from uuid import UUID
+from typing import Sequence
+from sqlalchemy import select
+
 from app.repositories.base import BaseRepo
 from app.database.models.trade import Trade
 
@@ -5,4 +9,13 @@ from app.database.models.trade import Trade
 class TradeRepo(BaseRepo):
     model = Trade
 
-    pass
+    async def get_all_for_system(self, system_id: UUID, user_id: UUID) -> Sequence[Trade]:
+        result = await self.session.execute(
+            select(Trade)
+            .where(
+                Trade.trading_system_id == system_id,
+                Trade.user_id == user_id
+            )
+        )
+
+        return result.scalars().all()
