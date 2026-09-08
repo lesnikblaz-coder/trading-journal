d9a6c4b2f60b82b7a4c9c59dc1c46e047ae28388
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-09-08 14:14:52 +0200

    remove unused exception

--- app/exceptions/custom.py
+++ app/exceptions/custom.py
@@ -33,7 +33,3 @@ class InvalidTradingSystemError(AppException):
 class InvalidTradeDataValuesError(AppException):
     status_code = 422
     detail = "Invalid trade data (entry/stop loss/exit price for trade direction)."
-
-class NoTradesFoundError(AppException):
-    status_code = 404
-    detail = "No trades found."
