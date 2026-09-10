1140dd4923dc8843e6120778e05fe92aeabfed8b
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-09-10 10:54:17 +0200

    add get_for_month query, to fetch all trades for given user for selected month

--- app/repositories/trade.py
+++ app/repositories/trade.py
@@ -1,6 +1,7 @@
 from uuid import UUID
 from typing import Sequence
 from sqlalchemy import select
+from datetime import datetime, timezone
 
 from app.repositories.base import BaseRepo
 from app.database.models.trade import Trade
@@ -15,6 +16,25 @@ class TradeRepo(BaseRepo):
             .where(
                 Trade.trading_system_id == system_id,
                 Trade.user_id == user_id
+            )
+        )
+
+        return result.scalars().all()
+
+    async def get_for_month(self, year: int, month: int, user_id: UUID) -> Sequence[Trade]:
+        start = datetime(year, month, 1, tzinfo=timezone.utc)
+
+        if month == 12:
+            end = datetime(year + 1, 1, 1, tzinfo=timezone.utc)
+        else:
+            end = datetime(year, month + 1, 1, tzinfo=timezone.utc)
+
+        result = await self.session.execute(
+            select(Trade)
+            .where(
+                Trade.user_id == user_id,
+                Trade.created_at >= start,
+                Trade.created_at < end
             )
         )
 
