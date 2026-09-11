cea347cb9a63a286fb4519e61d3539f81f696a62
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-09-11 19:08:59 +0200

    create a shared _issue_tokens function, used by login and register methods, so both adapt to the new TokenReposnse schema which returns both access and refresh token

--- app/services/auth.py
+++ app/services/auth.py
@@ -1,5 +1,9 @@
+from datetime import datetime, timezone, timedelta
+
 from app.core import security
+from app.core.config import settings
 from app.repositories.user import UserRepo
+from app.repositories.refresh_token import RefreshTokenRepo
 from app.exceptions.custom import DuplicateEmailError, InvalidCredentialsError
 from app.schemas.auth import TokenResponse
 from app.database.models.user import User
@@ -8,8 +12,29 @@ from app.exceptions.custom import UserNotFoundError
 
 
 class AuthService:
-    def __init__(self, repo: UserRepo):
+    def __init__(self, repo: UserRepo, refresh_repo: RefreshTokenRepo):
         self.repo = repo
+        self.refresh_repo = refresh_repo
+
+    async def _issue_tokens(self, user: User) -> TokenResponse:
+        access_token = security.create_access_token(user.id)
+
+        refresh_token = security.create_refresh_token()
+        refresh_token_hash = security.hash_refresh_token(refresh_token)
+
+        expire_at = datetime.now(timezone.utc) + timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS)
+
+        await self.refresh_repo.create(
+            user_id=user.id,
+            token_hash=refresh_token_hash,
+            expire_at=expire_at
+        )
+
+        return TokenResponse(
+            access_token=access_token,
+            refresh_token=refresh_token
+        )
+
 
     async def register(self, email: str, pw: str) -> TokenResponse:
         if await self.repo.get_by_email(email):
@@ -22,9 +47,7 @@ class AuthService:
 
         user = await self.repo.create(user)
 
-        return TokenResponse(
-            access_token=security.create_access_token(user.id)
-        )
+        return await self._issue_tokens(user)
 
     async def login(self, email: str, pw: str) -> TokenResponse:
         user = await self.repo.get_by_email(email)
@@ -32,9 +55,7 @@ class AuthService:
         if not user or not security.verify_pw(pw, user.hashed_pw):
             raise InvalidCredentialsError()
 
-        return TokenResponse(
-            access_token=security.create_access_token(user.id)
-        )
+        return await self._issue_tokens(user)
 
     async def decode_user(self, token: str) -> User | None:
         user_id = decode_access_token(token)
