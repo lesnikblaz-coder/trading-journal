705558d006bc42558c0dd417d5a9a9e773b6e804
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-09-17 13:41:04 +0200

    move files to dedicated folders

--- app/ai/ai_client.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from app.schemas.ai import AITradeReviewInput, AITradeReviewResponse
-
-
-class AIClient(ABC):
-    @abstractmethod
-    async def generate_trade_review(self, data: AITradeReviewInput) -> AITradeReviewResponse:
-        pass
