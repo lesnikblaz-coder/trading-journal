c28d7f3e5a5040e3ce8839bf8d3cb87c395cff95
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-09-23 16:01:19 +0200

    fix: add (now) required system_id to endpoint url and service method parameter

--- app/routers/analytics.py
+++ app/routers/analytics.py
@@ -12,6 +12,6 @@ router = APIRouter()
 async def analytics_overview(system_id: UUID, service: dep.AnalyticsServiceDep, user: dep.CurrentUserDep) -> sc.AnalyticsOverviewResponse:
     return await service.overview_by_system_id(trading_system_id=system_id, user_id=user.id)
 
-@router.post("/analytics/perfromance/month", response_model=sc.AnalyticsOverviewResponse)
-async def monthly_performance(service: dep.AnalyticsServiceDep, request: sc.PerformanceRequest, user: dep.CurrentUserDep) -> sc.AnalyticsOverviewResponse:
-    return await service.overview_monthly_performance(request.year, request.month, user.id)
+@router.post("/analytics/{system_id}/perfromance/month", response_model=sc.AnalyticsOverviewResponse)
+async def monthly_performance(system_id: UUID, service: dep.AnalyticsServiceDep, request: sc.PerformanceRequest, user: dep.CurrentUserDep) -> sc.AnalyticsOverviewResponse:
+    return await service.overview_monthly_performance(request.year, request.month, system_id, user.id)
